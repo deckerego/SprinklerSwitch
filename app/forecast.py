77c535c98cdad547af0d4321fea32c4727953a83
John Ellis <deckerego@gmail.com>
2015-08-27 18:39:31 -0400

    Working precipitation loading

--- app/forecast.py
+++ app/forecast.py
@@ -3,55 +3,55 @@ import dateutil.parser #dateutil.parser.parse(text)
 from xml.etree import ElementTree
 import sqlite3
 import logging
+import base64
 
 class Forecast(object):
     def __init__(self, lat, lon):
         self.url = "http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php?lat=%s&lon=%s&product=time-series&Unit=e" % (lat, lon)
         self.db_destroy = False
-        self.db_conn = sqlite3.connect('forecast.db')
+        self.db_name = base64.b64encode("%s,%s" % (lat, lon))
+        self.db_conn = sqlite3.connect("%s.db" % self.db_name)
 
     def __del__(self):
         self.db_conn.close()
 
-    def update(self):
-        resp = urllib.urlopen(self.url)
-        tree = ElementTree.parse(resp)
-        layouts = {}
-
-        # Create initial timesequence data
+    def __load_timetables(self, tree):
         cursor = self.db_conn.cursor()
+
         for time in tree.getroot().iter(tag="time-layout"):
             table = None
             sequence = -1
-            key = None
 
             for child in time.getchildren():
                 if child.tag == "layout-key":
                     table = child.text
-                    layouts[table] = {}
 
-                    if self.db_destroy:
-                        logging.warn("Dropping table %s" % table)
-                        cursor.execute("DROP TABLE IF EXISTS [%s]" % table)
-
-                    logging.debug("Creating table %s if it doesn't exist" % table)
-                    cursor.execute("CREATE TABLE IF NOT EXISTS [%s] (starttime text PRIMARY KEY, endtime text, precipitation real)" % table)
+                    logging.info("Re-creating table %s" % table)
+                    cursor.execute("DROP TABLE IF EXISTS [%s]" % table)
+                    cursor.execute("CREATE TABLE [%s] (sequence integer PRIMARY KEY, starttime text, endtime text)" % table)
 
                 elif child.tag == "start-valid-time":
                     sequence += 1
-                    key = child.text
-                    layouts[table][sequence] = key
 
-                    logging.debug("Inserting (Replacing) record %s in %s" % (key, table))
-                    cursor.execute("INSERT OR REPLACE INTO [%s] VALUES (?, null, null)" % table, [(key),])
+                    logging.debug("Inserting (Replacing) record %s in %s" % (sequence, table))
+                    cursor.execute("INSERT OR REPLACE INTO [%s] VALUES (?, ?, null)" % table, (sequence, child.text))
 
                 elif child.tag == "end-valid-time":
-                    logging.debug("Updating record %s with %s in %s" % (key, child.text, table))
-                    cursor.execute("UPDATE [%s] SET endtime=? where starttime=?" % table, (child.text, key))
+                    logging.debug("Updating record %s with %s in %s" % (sequence, child.text, table))
+                    cursor.execute("UPDATE [%s] SET endtime=? where sequence=?" % table, (child.text, sequence))
+
             self.db_conn.commit()
 
-        # Update with precipitation data
+    def __load_precipitation(self, tree):
         cursor = self.db_conn.cursor()
+
+        if self.db_destroy:
+            logging.warn("Dropping table precipitation")
+            cursor.execute("DROP TABLE IF EXISTS [precipitation]")
+
+        logging.debug("Creating table precipitation if it doesn't exist")
+        cursor.execute("CREATE TABLE IF NOT EXISTS [precipitation] (starttime text PRIMARY KEY, endtime text, precipitation real)")
+
         for precipitation in tree.getroot().iter(tag="precipitation"):
             table = precipitation.attrib['time-layout']
             precip_type = precipitation.attrib['type']
@@ -60,13 +60,24 @@ class Forecast(object):
             if precip_type == "liquid":
                 for value in precipitation.iter(tag="value"):
                     sequence += 1
-                    key = layouts[table][sequence]
 
-                    logging.debug("Updating record %s with %s in %s" % (key, value.text, table))
-                    cursor.execute("UPDATE [%s] SET precipitation=? where starttime=?" % table, (value.text, key))
+                    cursor.execute("SELECT starttime, endtime FROM [%s] where sequence=?" % table, (sequence,))
+                    starttime, endtime = cursor.fetchone()
+
+                    logging.debug("Inserting record %s with %s in precipitation" % (starttime, value.text))
+                    cursor.execute("INSERT OR REPLACE INTO [precipitation] VALUES (?, ?, ?)", (starttime, endtime, value.text))
                 self.db_conn.commit()
+
+    def update(self):
+        layouts = {}
+        cursor = self.db_conn.cursor()
+        resp = urllib.urlopen(self.url)
+        tree = ElementTree.parse(resp)
+
+        self.__load_timetables(tree)
+        self.__load_precipitation(tree)
 
     def read(self):
         cursor = self.db_conn.cursor()
-        for row in cursor.execute("SELECT * FROM [k-p1h-n34-22]"):
+        for row in cursor.execute("SELECT * FROM [precipitation]"):
             print row
--- app/test/weather_test.py
+++ app/test/weather_test.py
@@ -1,3 +1,5 @@
+import os,sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from forecast import Forecast
 
 forecast = Forecast(39.5119795, -82.1713632)
