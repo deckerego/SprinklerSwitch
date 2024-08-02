1029065a9651469ae3f4a703e11d17c59ffa831d
John Ellis <532789+deckerego@users.noreply.github.com>
2024-08-01 20:03:12 -0400

    Simplified installation script

--- installer.sh
+++ installer.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Ensure node is installed
--x /bin/node
+if [[ ! -x /bin/node ]]; then
+    echo "Node.JS is not installed - exiting!"
+    exit -1
+fi
 
 echo "Installing SprinklerSwitch"
-curl -o- https://github.com/deckerego/Macropad_4chord_MIDI/releases/download/latest/SprinklerSwitch.tar.xz
+curl -o- https://github.com/deckerego/SprinklerSwitch/releases/latest/download/SprinklerSwitch.tar.xz
 sudo tar xvJf SprinklerSwitch.tar.xz -C /
 sudo update-rc.d sprinkler defaults
 cd /opt/sprinker
