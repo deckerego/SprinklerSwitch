a6acdca17f3912c9e2881ddbef38bd954197e0f8
John Ellis <532789+deckerego@users.noreply.github.com>
2024-08-30 18:00:02 -0400

    Consider precipitableWater

--- __tests__/services/forecast.test.js
+++ __tests__/services/forecast.test.js
@@ -4,7 +4,10 @@ jest.mock("../../src/repositories/gfs.js");
 const configRepository = require("../../src/repositories/config.js");
 jest.mock("../../src/repositories/config.js");
 
-describe("Determine irrigation need", () => {
+gfsRepository.getPrecipitationRate.mockImplementation((lat, lon, metric) =>  Promise.resolve(mockPrecipitationRateData));
+gfsRepository.getPrecipitableWater.mockImplementation((lat, lon, metric) =>  Promise.resolve(mockPrecipitableWaterData));
+
+describe("Precipitation rate", () => {
   test("Too much rain", async () => {
     jest.useFakeTimers().setSystemTime(new Date('2024-08-20T13:30:00.000Z'));
 
@@ -13,11 +16,8 @@ describe("Determine irrigation need", () => {
         case 'latitude': return 47.6205099;
         case 'longitude': return -122.3518523;
         case 'precipitationRateThreshold': return 0.0002;
+        case 'precipitableWaterThreshold': return 0.0;
       };
-    });
-
-    gfsRepository.getPrecipitationRate.mockImplementation((lat, lon, metric) => {
-      return Promise.resolve(mockPrecipitationData);
     });
 
     const result = await forecastService.shouldIrrigate();
@@ -32,11 +32,8 @@ describe("Determine irrigation need", () => {
         case 'latitude': return 47.6205099;
         case 'longitude': return -122.3518523;
         case 'precipitationRateThreshold': return 0.0003;
+        case 'precipitableWaterThreshold': return Number.MAX_SAFE_INTEGER;
       };
-    });
-
-    gfsRepository.getPrecipitationRate.mockImplementation((lat, lon, metric) => {
-      return Promise.resolve(mockPrecipitationData);
     });
 
     const result = await forecastService.shouldIrrigate();
@@ -44,69 +41,234 @@ describe("Determine irrigation need", () => {
   });
 })
 
-const mockPrecipitationData = [
+describe("Precipitable water", () => {
+  test("Heavy clouds", async () => {
+    jest.useFakeTimers().setSystemTime(new Date('2024-08-20T13:30:00.000Z'));
+
+    configRepository.get.mockImplementation((key) => {
+      switch (key) {
+        case 'latitude': return 47.6205099;
+        case 'longitude': return -122.3518523;
+        case 'precipitationRateThreshold': return 0.0;
+        case 'precipitableWaterThreshold': return 50.0;
+      };
+    });
+
+    const result = await forecastService.shouldIrrigate();
+    expect(result).toBe(false);
+  });
+
+  test("No clouds", async () => {
+    jest.useFakeTimers().setSystemTime(new Date('2024-08-20T13:30:00.000Z'));
+
+    configRepository.get.mockImplementation((key) => {
+      switch (key) {
+        case 'latitude': return 47.6205099;
+        case 'longitude': return -122.3518523;
+        case 'precipitationRateThreshold': return Number.MAX_SAFE_INTEGER;
+        case 'precipitableWaterThreshold': return 46.0;
+      };
+    });
+
+    const result = await forecastService.shouldIrrigate();
+    expect(result).toBe(true);
+  });
+})
+
+const mockPrecipitationRateData = [
   {
     "time": new Date("2024-08-19T16:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0
   },
   {
     "time": new Date("2024-08-19T19:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0
   },
   {
     "time": new Date("2024-08-19T22:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0
   },
   {
     "time": new Date("2024-08-20T01:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0
   },
   {
     "time": new Date("2024-08-20T04:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0
   },
   {
     "time": new Date("2024-08-20T07:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0
   },
   {
     "time": new Date("2024-08-20T10:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0
   },
   {
     "time": new Date("2024-08-20T13:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0
   },
   {
     "time": new Date("2024-08-20T16:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0
   },
   {
     "time": new Date("2024-08-20T19:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0.000002940067597816433
   },
   {
     "time": new Date("2024-08-20T22:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0.00007464068029076087
   },
   {
     "time": new Date("2024-08-21T01:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0.00001905282479447121
   },
   {
     "time": new Date("2024-08-21T04:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0.00010161692339361316
   },
   {
     "time": new Date("2024-08-21T07:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0.00005031439416219344
   },
   {
     "time": new Date("2024-08-21T10:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0
   },
   {
     "time": new Date("2024-08-21T13:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
     "value": 0.0000013801752054913108
+  }
+];
+
+const mockPrecipitableWaterData = [
+  {
+    "time": new Date("2024-08-19T16:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 0
+  },
+  {
+    "time": new Date("2024-08-19T19:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 0
+  },
+  {
+    "time": new Date("2024-08-19T22:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 0
+  },
+  {
+    "time": new Date("2024-08-20T01:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 0
+  },
+  {
+    "time": new Date("2024-08-20T04:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 0
+  },
+  {
+    "time": new Date("2024-08-20T07:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 0
+  },
+  {
+    "time": new Date("2024-08-20T10:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 0
+  },
+  {
+    "time": new Date("2024-08-20T13:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 0
+  },
+  {
+    "time": new Date("2024-08-20T16:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 0
+  },
+  {
+    "time": new Date("2024-08-20T19:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 40.0
+  },
+  {
+    "time": new Date("2024-08-20T22:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 45.5
+  },
+  {
+    "time": new Date("2024-08-21T01:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 30.5
+  },
+  {
+    "time": new Date("2024-08-21T04:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 10.5
+  },
+  {
+    "time": new Date("2024-08-21T07:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 0
+  },
+  {
+    "time": new Date("2024-08-21T10:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 0
+  },
+  {
+    "time": new Date("2024-08-21T13:00:00.000Z"),
+    "latitude": 47.75,
+    "longitude": -122.25,
+    "value": 0
   }
 ];
--- src/services/forecast.js
+++ src/services/forecast.js
@@ -4,7 +4,6 @@ const configRepository = require("../repositories/config.js");
 class ForecastService {
     async shouldIrrigate() {
         const now = new Date();
-        const rainThreshold = configRepository.get("precipitationRateThreshold", 0.001);
         const latitude = configRepository.get("latitude");
         const longitude = configRepository.get("longitude");
 
@@ -12,29 +11,32 @@ class ForecastService {
         const priorAccumulation = precipitationRate.reduce((acc, result) => result.time <= now ? acc + result.value : acc, 0);
         const forecastAccumulation = precipitationRate.reduce((acc, result) => result.time > now ? acc + result.value : acc, 0);
         const totalAccumulation = priorAccumulation + forecastAccumulation;
-        console.info(`Total surface precipitation (kg/m^2/s) @[${precipitationRate[0].latitude}, ${precipitationRate[0].longitude}]: ${priorAccumulation} + ${forecastAccumulation} = ${totalAccumulation}`);
+        if(precipitationRate[0]) console.info(`Total surface precipitation (kg/m^2/s) @[${precipitationRate[0].latitude}, ${precipitationRate[0].longitude}]: ${priorAccumulation} + ${forecastAccumulation} = ${totalAccumulation}`);
 
         const precipitableWater = await gfsRepository.getPrecipitableWater(latitude, longitude) || [];
         const maxPrecipitable = precipitableWater.reduce((acc, result) => result.time > now && result.value > acc ? result.value : acc, 0);
-        console.info(`Maximum precipitable water (kg/m^2) @[${precipitableWater[0].latitude}, ${precipitableWater[0].longitude}]: ${maxPrecipitable}`);
+        if(precipitableWater[0]) console.info(`Maximum precipitable water (kg/m^2) @[${precipitableWater[0].latitude}, ${precipitableWater[0].longitude}]: ${maxPrecipitable}`);
 
         const cloudWater = await gfsRepository.getCloudWater(latitude, longitude) || [];
         const maxCloudWater = cloudWater.reduce((acc, result) => result.time > now && result.value > acc ? result.value : acc, 0);
-        console.info(`Maximum cloud water (kg/m^2) @[${cloudWater[0].latitude}, ${cloudWater[0].longitude}]: ${maxCloudWater}`);
+        if(cloudWater[0]) console.info(`Maximum cloud water (kg/m^2) @[${cloudWater[0].latitude}, ${cloudWater[0].longitude}]: ${maxCloudWater}`);
 
         const relativeHumidity = await gfsRepository.getRelativeHumidity(latitude, longitude) || [];
         const priorRelativeHumidity = relativeHumidity.reduce((acc, result) => result.time <= now && result.value < acc ? result.value : acc, Number.MAX_SAFE_INTEGER);
         const forecastRelativeHumidity = relativeHumidity.reduce((acc, result) => result.time > now && result.value < acc ? result.value : acc, Number.MAX_SAFE_INTEGER);
         const minRelativeHumidity = Math.min(priorRelativeHumidity, forecastRelativeHumidity);
-        console.info(`Least relative humidity 30-0 millibars above ground (%) @[${relativeHumidity[0].latitude}, ${relativeHumidity[0].longitude}]: MIN(${priorRelativeHumidity}, ${forecastRelativeHumidity}) = ${minRelativeHumidity}`);
+        if(relativeHumidity[0]) console.info(`Least relative humidity 30-0 millibars above ground (%) @[${relativeHumidity[0].latitude}, ${relativeHumidity[0].longitude}]: MIN(${priorRelativeHumidity}, ${forecastRelativeHumidity}) = ${minRelativeHumidity}`);
 
         const groundTemp = await gfsRepository.getGroundTemperature(latitude, longitude) || [];
         const priorGroundTemp = groundTemp.reduce((acc, result) => result.time <= now && result.value > acc ? result.value : acc, 0);
         const forecastGroundTemp = groundTemp.reduce((acc, result) => result.time > now && result.value > acc ? result.value : acc, 0);
         const avgGroundTemp = (priorGroundTemp + forecastGroundTemp) / 2;
-        console.info(`Average temperature 30-0 millibars above ground (k) @[${groundTemp[0].latitude}, ${groundTemp[0].longitude}]: AVG(${priorGroundTemp}, ${forecastGroundTemp}) = ${avgGroundTemp}`);
+        if(groundTemp[0]) console.info(`Average temperature 30-0 millibars above ground (k) @[${groundTemp[0].latitude}, ${groundTemp[0].longitude}]: AVG(${priorGroundTemp}, ${forecastGroundTemp}) = ${avgGroundTemp}`);
 
-        return totalAccumulation <= rainThreshold;
+        const precipitationRateThreshold = configRepository.get("precipitationRateThreshold", 0.001);
+        const precipitableWaterThreshold = configRepository.get("precipitableWaterThreshold", 50.0);
+        return (totalAccumulation <= precipitationRateThreshold)
+            && (maxPrecipitable < precipitableWaterThreshold);
     }
 }
 
