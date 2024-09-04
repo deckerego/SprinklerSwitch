b7186777ef5bd2a7289ae7f6ab8702f601e1b5ed
John Ellis <532789+deckerego@users.noreply.github.com>
2024-09-04 15:24:35 -0400

    Add cloud water threshold

--- src/services/rules.js
+++ src/services/rules.js
@@ -1,13 +1,23 @@
 const configRepository = require("../repositories/config.js");
 
 class RulesService {
-    async evaluate(facts) {
-        const precipitationRateThreshold = configRepository.get("precipitationRateThreshold", 0.001);
-        const precipitableWaterThreshold = configRepository.get("precipitableWaterThreshold", 50.0);
+    precipitationRateThreshold = configRepository.get("precipitationRateThreshold", 10.0);
+    precipitableWaterThreshold = configRepository.get("precipitableWaterThreshold", 50.0);
+    cloudWaterThreshold = configRepository.get("cloudWaterThreshold", 1.0);
+
+    evaluate(facts) {
         return !(
-            ((facts.priorAccumulation + facts.forecastAccumulation) > precipitationRateThreshold) &&
-            (facts.maxPrecipitable > precipitableWaterThreshold)
+            exceedsPrecipitationThreshold(facts.priorAccumulation, facts.forecastAccumulation) &&
+            exceedsWaterThreshold(facts.maxPrecipitable, facts.maxCloudWater)
         );
+    }
+
+    exceedsWaterThreshold(preciptable, cloudWater) {
+        return preciptable > precipitableWaterThreshold || cloudWater > cloudWaterThreshold
+    }
+
+    exceedsPrecipitationThreshold(priorAccumulation, forecastAccumulation) {
+        return (priorAccumulation + forecastAccumulation) > precipitationRateThreshold
     }
 }
 
