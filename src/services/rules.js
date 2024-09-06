6f6c12b4a1e9b742b1efb4b669da518357db7063
John Ellis <532789+deckerego@users.noreply.github.com>
2024-09-06 14:54:03 -0400

    New default precipitation rate threshold

--- src/services/rules.js
+++ src/services/rules.js
@@ -1,7 +1,7 @@
 const configRepository = require("../repositories/config.js");
 
 class RulesService {
-    precipitationRateThreshold = configRepository.get("precipitationRateThreshold", 10.0);
+    precipitationRateThreshold = configRepository.get("precipitationRateThreshold", 5.0);
     precipitableWaterThreshold = configRepository.get("precipitableWaterThreshold", 50.0);
     cloudWaterThreshold = configRepository.get("cloudWaterThreshold", 1.0);
 
